f5834aea698ac9042fbd0662f9274d9d33288720
Simon Vrana <simon.vrana.pro@gmail.com>
2026-04-18 16:51:57 +0200

    [Fct] Bullet and Enemy prefab.

--- UnityProject/Assets/Scripts/Player/PlayerController.cs
+++ UnityProject/Assets/Scripts/Player/PlayerController.cs
@@ -3,9 +3,6 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private CharacterController controller;
-    private PlayerInput playerInput;
-
 	[SerializeField]
 	private GameObject body;
 
@@ -15,209 +12,213 @@ public class PlayerController : MonoBehaviour
 	[SerializeField]
 	private GameObject pewpew;
 
+	[SerializeField] private PlayerDamagable playerDamagable;
+
 	//GameData SO 
 	[SerializeField]
-    private SO_Float so_Speed;
-    [SerializeField]
-    private SO_bool so_controlDevice; //false = keyboard/mouse ; true = controller
-    [SerializeField]
-    private SO_Float so_Angle; // Angle to face mouse raycast position on a plane
-    [SerializeField] SO_ProjectileData so_ProjData;
+	private SO_Float so_Speed;
 
-    [SerializeField]
-    private GameObject projectilePrefab;
+	[SerializeField]
+	private SO_bool so_controlDevice; //false = keyboard/mouse ; true = controller
 
-    private float m_speed = 5;
-    [SerializeField]
-    private float jumpHeight = 2;
+	[SerializeField]
+	private SO_Float so_Angle; // Angle to face mouse raycast position on a plane
 
-    private float gravity = -9.8f;
+	[SerializeField] private SO_ProjectileData so_ProjData;
 
-    private bool isLookAtCursor = false;
-    private bool isMove = false;
-    private bool LastDirInput = false; // false = move; true = look
+	[SerializeField]
+	private PlayerBullet projectilePrefab;
 
-    private Vector2 moveInput;
-    private Vector3 velocity;
-    private Vector2 lookInput;
+	[SerializeField]
+	private float jumpHeight = 2;
 
-    // Direction Vectors for look/target and move
-    [SerializeField]
-    private Vector2 LookDirection = Vector2.zero;
-    [SerializeField]
-    private Vector2 lastMoveDir = Vector2.zero;
+	// Direction Vectors for look/target and move
+	[SerializeField]
+	private Vector2 LookDirection = Vector2.zero;
 
-    [SerializeField]
-    private GameObject targetArrow;
+	[SerializeField]
+	private Vector2 lastMoveDir = Vector2.zero;
 
-    private InputActionMap actionMap;
+	[SerializeField]
+	private GameObject targetArrow;
 
-    private void Awake()
-    {
-        controller = this.gameObject.GetComponent<CharacterController>();
-        playerInput = this.gameObject.GetComponent<PlayerInput>();
-        actionMap = playerInput.actions.FindActionMap("Player");
-    }
+	private readonly float gravity = -9.8f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+	private InputActionMap actionMap;
+	private CharacterController controller;
 
-    private void OnEnable()
-    {
-        so_Speed.OnValueChanged += changeSpeed;
+	private int fireCnt;
 
-        // register to actions
-        actionMap.FindAction("Target").started += OnTarget;
-        actionMap.FindAction("Scroll").started += OnScroll;
-    }
+	private bool isLookAtCursor;
+	private bool isMove;
+	private bool LastDirInput; // false = move; true = look
+	private Vector2 lookInput;
 
-    private void OnDisable()
-    {
-        so_Speed.OnValueChanged -= changeSpeed;
+	private float m_speed = 5;
 
-        // unregister to actions
-        actionMap.FindAction("Target").started -= OnTarget;
-		actionMap.FindAction("Scroll").started -= OnScroll;
+	private Vector2 moveInput;
+	private PlayerInput playerInput;
+	private Vector3 velocity;
 
+	private void Awake()
+	{
+		controller = gameObject.GetComponent<CharacterController>();
+		playerInput = gameObject.GetComponent<PlayerInput>();
+		actionMap = playerInput.actions.FindActionMap("Player");
+	}
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	private void Start()
+	{
 	}
 
 	// Update is called once per frame
-	void Update()
-    {
-        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
-        controller.Move(move * m_speed * Time.deltaTime);
+	private void Update()
+	{
+		Vector3 move = new(moveInput.x, 0, moveInput.y);
+		controller.Move(move * m_speed * Time.deltaTime);
 
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
-        
-        if(!so_controlDevice.Get())
-        {
-                isLookAtCursor = true;
-                targetArrow.SetActive(true);
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		velocity.y += gravity * Time.deltaTime;
+		controller.Move(velocity * Time.deltaTime);
 
-                // ininite horizontal plane at player height
-                Plane plane = new Plane(Vector3.up, transform.position);
+		if (!so_controlDevice.Get())
+		{
+			isLookAtCursor = true;
+			targetArrow.SetActive(true);
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-                // Check hit point and get direction
-                if (plane.Raycast(ray, out float distance))
-                {
-                    Vector3 hitPoint = ray.GetPoint(distance);
-                    Vector3 dir = hitPoint - transform.position;
+			// ininite horizontal plane at player height
+			Plane plane = new(Vector3.up, transform.position);
 
-                    Vector2 direction = new Vector2(dir.x, dir.z);
+			// Check hit point and get direction
+			if (plane.Raycast(ray, out float distance))
+			{
+				Vector3 hitPoint = ray.GetPoint(distance);
+				Vector3 dir = hitPoint - transform.position;
 
-                    LookDirection = direction.normalized;
+				Vector2 direction = new(dir.x, dir.z);
 
-                    LastDirInput = true;
-                }
-        }
+				LookDirection = direction.normalized;
 
-        if(!isLookAtCursor && isMove) // if just move use LastMoveDir
-        {
-            BodyDirection(lastMoveDir);
-        }
-        else if(!isLookAtCursor && !isMove)  // if target / Look action use LookDirection
-        {
-            if(LastDirInput)
-            {
-                AntennaDirection(LookDirection);
-            }
-            else
-            {
-                BodyDirection(lastMoveDir);
-            }
-        }
-        else
-        {
-            AntennaDirection(LookDirection);
+				LastDirInput = true;
+			}
+		}
+
+		if (!isLookAtCursor && isMove) // if just move use LastMoveDir
+		{
 			BodyDirection(lastMoveDir);
-
+		}
+		else if (!isLookAtCursor && !isMove) // if target / Look action use LookDirection
+		{
+			if (LastDirInput)
+			{
+				AntennaDirection(LookDirection);
+			}
+			else
+			{
+				BodyDirection(lastMoveDir);
+			}
+		}
+		else
+		{
+			AntennaDirection(LookDirection);
+			BodyDirection(lastMoveDir);
 		}
 	}
 
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        isMove = true;
-        moveInput = context.ReadValue<Vector2>();
-        //Debug.Log($"Move input : {moveInput}");
+	private void OnEnable()
+	{
+		so_Speed.OnValueChanged += changeSpeed;
 
-        if(moveInput != Vector2.zero)
-        {
-            lastMoveDir = moveInput;
-            LastDirInput = false;
-        }
-        else
-        {
-            isMove = false;
-        }
-    }
+		// register to actions
+		actionMap.FindAction("Target").started += OnTarget;
+		actionMap.FindAction("Scroll").started += OnScroll;
+	}
+
+	private void OnDisable()
+	{
+		so_Speed.OnValueChanged -= changeSpeed;
+
+		// unregister to actions
+		actionMap.FindAction("Target").started -= OnTarget;
+		actionMap.FindAction("Scroll").started -= OnScroll;
+	}
+
+	public void OnMove(InputAction.CallbackContext context)
+	{
+		isMove = true;
+		moveInput = context.ReadValue<Vector2>();
+		//Debug.Log($"Move input : {moveInput}");
+
+		if (moveInput != Vector2.zero)
+		{
+			lastMoveDir = moveInput;
+			LastDirInput = false;
+		}
+		else
+		{
+			isMove = false;
+		}
+	}
 
 
-    public void OnLook(InputAction.CallbackContext context)
-    {
-        if(so_controlDevice.Get()) //false = keyboard/mouse ; true = controller
-        {
-            lookInput = context.ReadValue<Vector2>();
-            if (lookInput != Vector2.zero)
-            {
-                LookDirection = lookInput;
-                isLookAtCursor = true;
-                LastDirInput = true;
-                targetArrow.SetActive(true);
-            }
-            else
-            {
-                isLookAtCursor = false;
-                targetArrow.SetActive(false);
-            }
-        }
-    }
-    
-    private int fireCnt = 0;
+	public void OnLook(InputAction.CallbackContext context)
+	{
+		if (so_controlDevice.Get()) //false = keyboard/mouse ; true = controller
+		{
+			lookInput = context.ReadValue<Vector2>();
+			if (lookInput != Vector2.zero)
+			{
+				LookDirection = lookInput;
+				isLookAtCursor = true;
+				LastDirInput = true;
+				targetArrow.SetActive(true);
+			}
+			else
+			{
+				isLookAtCursor = false;
+				targetArrow.SetActive(false);
+			}
+		}
+	}
 
-    public void OnFire(InputAction.CallbackContext context)
-    {
-        //Debug.Log("Fire");
+	public void OnFire(InputAction.CallbackContext context)
+	{
+		//Debug.Log("Fire");
 
-        fireCnt++;
-        if(fireCnt%3 == 0)
-        {
-            //Debug.Log("End Fire");
-            
-            fireCnt = 0;
+		fireCnt++;
+		if (fireCnt % 3 == 0)
+		{
+			//Debug.Log("End Fire");
 
-            //Test Fire
+			fireCnt = 0;
 
-            GameObject pew = Instantiate(projectilePrefab, pewpew.transform.position, projectileDirection(LookDirection));
-            //pew.GetComponent<BaseProjectile>().SetValues(so_ProjData.projectileBody, so_ProjData.speed, so_ProjData.dmg, so_ProjData.range, so_ProjData.impactRadius);
+			//Test Fire
 
-        }
-        else
-        {
-            //Debug.Log("Active  Fire");
-        }
-    }
+			PlayerBullet pew = Instantiate(projectilePrefab, pewpew.transform.position,
+				projectileDirection(LookDirection));
+			pew.Frequency = playerDamagable.Frequency;
+			//pew.GetComponent<BaseProjectile>().SetValues(so_ProjData.projectileBody, so_ProjData.speed, so_ProjData.dmg, so_ProjData.range, so_ProjData.impactRadius);
+		}
+		//Debug.Log("Active  Fire");
+	}
 
-    private void BodyDirection()
-    {
-        //Get angle from LookDirection
-        float targetAngle = Mathf.Atan2(LookDirection.x, LookDirection.y) * Mathf.Rad2Deg;
+	private void BodyDirection()
+	{
+		//Get angle from LookDirection
+		float targetAngle = Mathf.Atan2(LookDirection.x, LookDirection.y) * Mathf.Rad2Deg;
 
-        body.transform.localRotation = Quaternion.Euler(0f, targetAngle, 0f);
-    }
+		body.transform.localRotation = Quaternion.Euler(0f, targetAngle, 0f);
+	}
 
-    private void BodyDirection(Vector2 dir)
-    {
-        float targetAngle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
+	private void BodyDirection(Vector2 dir)
+	{
+		float targetAngle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
 
-        body.transform.localRotation = Quaternion.Euler(0f, targetAngle, 0f);
+		body.transform.localRotation = Quaternion.Euler(0f, targetAngle, 0f);
 
-        so_Angle.Set(targetAngle);
-    }
+		so_Angle.Set(targetAngle);
+	}
 
 	private void AntennaDirection(Vector2 dir)
 	{
@@ -235,48 +236,46 @@ public class PlayerController : MonoBehaviour
 		return Quaternion.Euler(0f, targetAngle, 0f);
 	}
 
-    private void changeSpeed(float p_speed)
-    {
-        m_speed = p_speed;
-    }
+	private void changeSpeed(float p_speed)
+	{
+		m_speed = p_speed;
+	}
 
-#region mouse/keyboard specifics
+	#region mouse/keyboard specifics
 
-    //test
-    private void OnTarget(InputAction.CallbackContext context)
-    {
-        //Debug.LogWarning("target mouse");
+	//test
+	private void OnTarget(InputAction.CallbackContext context)
+	{
+		//Debug.LogWarning("target mouse");
 
-        //lastMoveDir = LookDirection;
-    }
+		//lastMoveDir = LookDirection;
+	}
 
 	private void OnScroll(InputAction.CallbackContext context)
 	{
 		//Debug.Log(" test" + context.ReadValue<Vector2>());
 		//TODO change frequence 
 
+		playerDamagable.Frequency = playerDamagable.Frequency switch
+		{
+			Frequencies.Square => Frequencies.Triangle,
+			Frequencies.Triangle => Frequencies.Wave,
+			_ => Frequencies.Square
+		};
 	}
 
-    private void MouseTarget()
-    {
-        if (actionMap.FindAction("Target").IsPressed())
-        {
-            isLookAtCursor = true;
-            // use angle to set direction
-        }
-        else
-        {
-            isLookAtCursor = false;
-        }
-    }
+	private void MouseTarget()
+	{
+		if (actionMap.FindAction("Target").IsPressed())
+		{
+			isLookAtCursor = true;
+			// use angle to set direction
+		}
+		else
+		{
+			isLookAtCursor = false;
+		}
+	}
 
-    #endregion
-
-
-#region fire
-
-
-    
-#endregion
-
+	#endregion
 }

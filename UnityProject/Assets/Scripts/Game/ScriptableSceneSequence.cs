5d98cb1b8d191286f7225677031a953d366488c5
ValentinWB <valentinwantzbessou@gmail.com>
2026-04-20 00:55:00 +0200

    Minor fixes avant la rupture d'anevrisme

--- UnityProject/Assets/Scripts/Game/ScriptableSceneSequence.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ScriptableSceneSequence", menuName = "Scriptable Objects/ScriptableSceneSequence")]
-public class ScriptableSceneSequence : ScriptableObject
-{
-    
-}
